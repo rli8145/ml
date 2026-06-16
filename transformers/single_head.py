04cd7720387e4c2d4a21703f8c2d9a90bc11229b
Ryan Li <116306885+rli8145@users.noreply.github.com>
2026-06-16 13:57:01 -0400

    docs: add to single-head attention comments

--- transformers/single_head.py
+++ transformers/single_head.py
@@ -3,20 +3,22 @@
 # T = num_tokens/seq_length, d = embedding_dim/model_dim
 # each head of attention has W_Q, W_K, W_V. (d, d_k), d_k = attention_dim 
 # X is (T, d)
-# Q = X @ W_Q, K = X @ W_K, V = X @ W_V. (T, d) @ (d, d_k) -> (T, d_k) i.e. each token is projected into a query/key/value vector
-# K, Q are determine attention weights via Q @ K.T and are symmetric in role (could swap and get same weights but transposed)
-# V extracts value vector for each token once attention is decided
+# Q = X @ W_Q, K = X @ W_K, V = X @ W_V. (T, d) @ (d, d_k) -> (T, d_k) i.e. each token is mapped onto a query/key/value vector
+# K, Q determine attention weights via Q @ K.T and are symmetric in role (could swap and get same weights but transposed). row i, col j = how much token i attends to token j in the context of the this head
+# V extracts value vector for each token once weights are decided
+# search engine analogy: K, Q gives you relevance scores, V gives you actual content
 
 # Attention:
-# Q @ K.T:              (T, d_k) @ (d_k, T) -> (T, T) matrix of dot products!
-# softmax(QK.T/√dk) (T, T) - dividing by √dk normalizes variance. axis=1, i.e. softmax each row individually
-# output:            softmax(Q @ K.T / √dk) @ V = (T, T) @ (T, d_k) -> (T, d_k)
+# Q @ K.T:  (T, d_k) @ (d_k, T) -> (T, T) matrix of dot products!
+# weights: softmax(QK.T/√dk) (T, T) - dividing by √dk normalizes variance. axis=1, i.e. softmax each row 
+# output:  weights @ V = (T, T) @ (T, d_k) -> (T, d_k). for each of the d_k dimensions, take the corresponding values from each token (T,) and take T weighted sums with the softmaxed dot products
 
 
 # notes
-# - intuition: embedding diff as a function of context
+# - intuition: new token embedding as a function of context
 # - masking to prevent later tokens from influencing earlier ones
-# - attention pattern size = context size ^ 2. bottleneck
+# - attention pattern ((T, T) weight matrix) size = T^2. bottleneck which motivates sparse attention, flash attention, etc
+# - backprop trains everything from W_O, W_V, W_Q, W_K to embedding table (vocab_size, d). error = cross-entropy loss
 
 import torch
 import torch.nn as nn

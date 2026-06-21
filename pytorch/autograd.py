8d37510426141f7f4a60a5569e231cac0d4f7293
rli8145 <rli8145@gmail.com>
2026-06-20 20:20:35 -0400

    more autograd comments

--- pytorch/autograd.py
+++ pytorch/autograd.py
@@ -7,14 +7,14 @@ import numpy as np
 # forward
 # z = x * y + x.relu()
 #
-# backward — computes gradients (loss.backward() in pytorch)
+# backward — computes gradients (i.e. loss.backward() in pytorch)
 # z.backward()
 #
-# update weights (optimizer.step() in pytorch)
+# update weights (i.e. optimizer.step() in pytorch)
 # x.data -= lr * x.grad
 # y.data -= lr * y.grad
 #
-# zero gradients for next iter (optimizer.zero_grad() in pytorch)
+# zero gradients for next iteration (i.e. optimizer.zero_grad() in pytorch)
 # x.grad = 0.0
 # y.grad = 0.0
 
@@ -25,16 +25,16 @@ class Tensor:
         self.grad = 0.0
         self._backward = lambda: None  # how to compute local gradient
         self._prev = set(_children) # inputs to this node
-        self._op = _op
+        self._op = _op # for debugging
 
-    # z = h(f(x), g(x)): ∂z/∂x = ∂z/∂f * ∂f/∂x + ∂z/∂g * ∂g/∂x
     def __add__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.data + other.data, (self, other), '+')
 
+        # ∂L/∂self += ∂out/∂self * ∂L/∂out. ∂out/∂self = 1
         def _backward():
-            self.grad += out.grad # ∂/∂x (x+y) = 1
-            other.grad += out.grad # ∂/∂y (x+y) = 1
+            self.grad += out.grad 
+            other.grad += out.grad
         out._backward = _backward
         return out
 
@@ -42,9 +42,10 @@ class Tensor:
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.data * other.data, (self, other), '*')
 
+        # ∂L/∂self += ∂out/∂self * ∂L/∂out. ∂out/∂self = other
         def _backward():
-            self.grad += other.data * out.grad # ∂/∂x (x*y) = y
-            other.grad += self.data * out.grad # ∂/∂y (x*y) = x
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
         out._backward = _backward
         return out
     
@@ -70,8 +71,25 @@ class Tensor:
         topo(self)
 
         self.grad = 1.0  # dL/dL = 1
-        for node in reversed(nodes):
+        for node in reversed(nodes): # parents before children
             node._backward()
 
     def __repr__(self):
         return f"Tensor(data={self.data}, grad={self.grad})"
+
+# += in _backward() needed to handle graphs with shared nodes. if multiple paths to z from x: 
+# z = h(f(x), g(x)) -> ∂z/∂x = ∂z/∂f * ∂f/∂x + ∂z/∂g * ∂g/∂x
+
+# Example:
+# x = Tensor(3.0)
+# z = x * x
+# z.backward()
+# print(x.grad)
+
+# x → (self)  ↘
+#              * → z
+# x → (other) ↗
+
+# Vanilla neural network with distinct weights: x → W1 → ReLU → W2 → ReLU → W3 → ReLU → ... → Wn → preds
+# each Wi is used exactly once, so each Wi.grad gets += exactly once during backward.
+# if some W repeated, then += needed for accumulation
